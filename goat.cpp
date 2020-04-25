f163265559b4d700b115f27f541f60f32b2c2473
William Woodruff <william@trailofbits.com>
2020-04-24 21:55:55 -0400

    goat: Add a basic stack buffer overflow

--- goat.cpp
+++ goat.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int buffer_overflow(void) {
+    char buf[10];
+    scanf("%s", buf);
+    return 1;
+}
+
 int main(int argc, char *argv[], char *envp[]) {
     std::cout << "baa!" << std::endl;
+    return buffer_overflow();
 }
